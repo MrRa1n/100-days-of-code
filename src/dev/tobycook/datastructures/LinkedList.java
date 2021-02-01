986538fa169a67e37985868ee6db484003a8c730
Toby Cook <tobycookni@gmail.com>
2021-02-01 20:26:01 +0000

    Added basic linked list class

--- src/dev/tobycook/datastructures/LinkedList.java
+++ src/dev/tobycook/datastructures/LinkedList.java
@@ -11,5 +11,32 @@ class Node {
 }
 
 public class LinkedList {
+  Node head;
 
+  public LinkedList() {
+    this.head = null;
+  }
+
+  public void display() {
+    Node temp = head;
+    while (temp != null) {
+      System.out.print(temp.data + " ");
+      temp = temp.next;
+    }
+  }
+
+  public static void main(String[] args) {
+    LinkedList list = new LinkedList();
+    list.head = new Node(0);
+
+    Node node1 = new Node(1);
+    Node node2 = new Node(2);
+    Node node3 = new Node(3);
+
+    list.head.next = node1;
+    node1.next = node2;
+    node2.next = node3;
+
+    list.display();
+  }
 }
